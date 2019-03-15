3b6af449806c055e4e6e3aad0ee7fe9132bad6bd
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-03-14 23:27:22 -0700

    try to set up netlify CMS in production

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,17 @@
       </div>
     </div>
   </section>
+  <script>
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", user => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+</script>
 </template>
 
 <script>
