97581dd13599126d266ed052eb9f300dc797273a
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-03-15 20:11:07 -0700

    style guide additions

--- pages/guide.vue
+++ pages/guide.vue
@@ -2,13 +2,20 @@
   <section>
     <h1>Style Guide</h1>
 
-    <h2>Text Styles</h2>
+    <h2>Text Elements</h2>
     <sgItem selector="h1" sampleCode="<h1>Hello, World.</h1>" />
     <sgItem selector="h2" sampleCode="<h2>Hello, World.</h2>" />
     <sgItem selector="h3" sampleCode="<h3>Hello, World.</h3>" />
     <sgItem selector="h4" sampleCode="<h4>Hello, World.</h4>" />
     <sgItem selector="h5" sampleCode="<h5>Hello, World.</h5>" />
     <sgItem selector="h6" sampleCode="<h6>Hello, World.</h6>" />
+
+    <h2>Interactive Elements</h2>
+    <sgItem selector="nav a">
+      <code>
+        
+      </code>
+    </sgItem>
 
   </section>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <section>
     <h1>Work</h1>
     <div id="projects">
-      <div class="project" v-for="project in projects">
+      <nuxt-link v-for="project in projects" v-bind:key="project.title" class="project" :to="project.permalink">
         <img :src="project.thumbnail" :alt="project.title">
-        <nuxt-link :to="project.permalink">{{ project.title }}</nuxt-link>
-      </div>
+        <h3>{{ project.title }}</h3>
+      </nuxt-link>
     </div>
   </section>
 </template>
@@ -33,5 +33,9 @@ export default {
 .project img {
   display: block;
   max-width: 100%;
+}
+
+.project nuxt-link {
+
 }
 </style>
