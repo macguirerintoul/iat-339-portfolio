36ba52c591a3ac4eaddd929fe8f577a60824f597
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-03-19 12:51:09 -0700

    extract project to component

---
+++ components/project.vue
@@ -0,0 +1,18 @@
+<template>
+  <nuxt-link v-bind:key="project.title" class="project" :to="project.permalink">
+    <img :src="project.thumbnail" :alt="project.title">
+    <h3>{{ project.title }}</h3>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    project: Object
+  },
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,18 @@
   <section>
     <h1>Work</h1>
     <div id="projects">
-      <nuxt-link v-for="project in projects" v-bind:key="project.title" class="project" :to="project.permalink">
-        <img :src="project.thumbnail" :alt="project.title">
-        <h3>{{ project.title }}</h3>
-      </nuxt-link>
+      <project v-for="project in projects" :project="project" />
     </div>
   </section>
 </template>
 
 <script>
+import project from '~/components/project.vue';
+
 export default {
+  components: {
+    project
+  },
   async asyncData({app}) {
     return {
       projects: await app.$content('projects').getAll()
