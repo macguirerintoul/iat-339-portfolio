a8080149832f5da6ed9e79e5c2c07fb82d9dde34
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-03-26 12:08:15 -0700

    mobile nav

---
+++ components/NavigationMobile.vue
@@ -0,0 +1,49 @@
+<template>
+<nav class="navbar is-fixed-top">
+  <div class="navbar-brand">
+    <a aria-current="page" class="navbar-item" href="/">
+      <h1 class="title is-5">Macguire Rintoul</h1>
+    </a>
+    <div v-on:click="toggleMobileMenu" class="navbar-burger burger" data-target="navMenu" aria-label="menu" aria-expanded="false">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </div>
+  </div>
+  <div id="navMenu" :class="mobileMenuClass">
+    <div class="navbar-end">
+      <a class="navbar-item" href="/">Home</a>
+      <a class="navbar-item" href="/about">About</a>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      showMobileMenu: false
+    }
+  },
+  methods: {
+    toggleMobileMenu: function() {
+      console.log('Toggling mobile menu');
+      this.showMobileMenu = !this.showMobileMenu;
+    }
+  },
+  computed: {
+    mobileMenuClass: function () {
+      return 'navbar-menu ' + (this.showMobileMenu ? 'is-active' : '');
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// Navbar CSS from Bulma
+// https://bulma.io/
+@import "../node_modules/bulma/sass/utilities/_all";
+@import "../node_modules/bulma/sass/base/_all";
+@import "../node_modules/bulma/sass/components/navbar";
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
+<div>
+  <NavigationMobile />
+
   <div id="container">
     <div id="sidebar">
-      <h2 id="name"><nuxt-link to="/">Macguire Rintoul</nuxt-link></h2>
+      <h2 id="name">
+        <nuxt-link to="/">Macguire Rintoul</nuxt-link>
+      </h2>
       <h4>Experience designer & developer</h4>
       <nav>
         <nuxt-link to="/">Work</nuxt-link>
@@ -13,7 +18,18 @@
       <h6>&copy; 2019 Macguire Rintoul</h6>
     </div>
     <div id="content">
-      <nuxt/>
+      <nuxt />
     </div>
   </div>
+</div>
 </template>
+
+<script>
+import NavigationMobile from '~/components/NavigationMobile.vue';
+
+export default {
+  components: {
+    NavigationMobile
+  }
+}
+</script>
