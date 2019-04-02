e64f9f998a870d487490105fa02463b42a19c4c0
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-04-02 11:01:24 -0700

    add photo to about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,9 +2,17 @@
   <section>
     <h1>Hire Me</h1>
 
-    <p>I'm Macguire: an experience designer, full-stack web developer, hackathon participant, music producer, and Interactive Arts & Technology undergraduate at Simon Fraser University in Vancouver, BC. I would love to work for a large tech company, or a small startup doing either design or development, as I have a passion for both.</p>
+    <div class="showcase">
+      <div>
+        <p>I'm Macguire: an experience designer, full-stack web developer, hackathon participant, music producer, and Interactive Arts & Technology undergraduate at Simon Fraser University in Vancouver, BC. I would love to work for a large tech company, or a small startup doing either design or development, as I have a passion for both.</p>
+        <p>When I'm not studying or learning a new web framework, you can catch me DJing, making my own beats, editing travel videos, or hiking the Swiss Alps with my mom.</p>
+      </div>
+      <div>
+        <img src="/images/profile.jpg" alt="A photo of Macguire Rintoul.">
+      </div>
+    </div>
 
-    <p>When I'm not studying or learning a new web framework, you can catch me DJing, making my own beats, editing travel videos, or hiking the Swiss Alps with my mom.</p>
+
   </section>
 </template>
 
